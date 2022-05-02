43fe602fe5a7b6d48b9977092502ecfee8dab385
kraused53 <kraused53@gmail.com>
2022-05-02 02:04:21 -0400

    Was able to make icons display on screen

--- notifications.py
+++ notifications.py
@@ -1,4 +1,5 @@
 from location import get_location
+from openweather import get_current_weather
 from PIL import Image, ImageFont, ImageDraw
 from inky import InkyWHAT
 from datetime import datetime
@@ -9,13 +10,55 @@ if __name__ == '__main__':
 	display = InkyWHAT("red")
 	display.set_border(display.WHITE)
 
-	icon = Image.open(icon_dir+'thunderstorm.png', mode='r').convert('P')
-	img = Image.new('P', (display.WIDTH, display.HEIGHT), (255, 255, 255))
+	img = Image.new('RGBA', (400, 300), (255, 255, 255))
+	icon = Image.open('./icons/thunderstorm.png').convert('RGBA')
+
+	pixels = icon.load()
+
+	if icon.mode == 'RGBA':
+		for y in range(icon.size[1]):
+			for x in range(icon.size[0]):
+				if pixels[x, y][3] < 255:
+					pixels[x, y] = (255, 255, 255, 255)
+
+	img.paste(icon, (0, 0))
+
+	pal_img = Image.new('P', (1, 1))
+	pal_img.putpalette((255, 255, 255, 0, 0, 0, 255, 0, 0) + (0, 0, 0) * 252)
+
+	img = img.convert('RGB').quantize(palette=pal_img)
+
+	display.set_image(img)
+	display.show()
+'''
+	# Load test icon
+	icon = Image.open(icon_dir+'thunderstorm.png')
+	#img = Image.open(icon_dir+'background.png')
+	img = Image.new('RGBA', (400, 300), (255, 255, 255))
+
 	draw = ImageDraw.Draw(img)
 
-	font = ImageFont.truetype("DejaVuSans.ttf", 12)
+	Image.Image.paste(img, icon, (0, 50))
+
+	font = ImageFont.truetype("DejaVuSans.ttf", 16)
 
 	loc = get_location()
+
+#	weather_data = get_current_weather(loc[1])
+#	print(weather_data)
+	weather_data = []
+	# Extract current temp
+	if 'main' in weather_data:
+		if 'temp' in weather_data['main']:
+			temp = str(weather_data['main']['temp']) + ' °F'
+		else:
+			temp = 'ERR'
+	else:
+			temp = 'ERR'
+
+#	img.save('test.png')
+	font = ImageFont.truetype("DejaVuSans.ttf", 24)
+	draw.text((130, 75), temp, display.BLACK, font)
 
 	message = loc[0]
 
@@ -26,20 +69,24 @@ if __name__ == '__main__':
 	draw.line([(0, 22), (display.WIDTH, 22)], fill=(0, 0, 0))
 	x = (display.WIDTH - w)
 
+	font = ImageFont.truetype("DejaVuSans.ttf", 16)
+
 	draw.text((22, 2), message, display.RED, font)
 	draw.text((x, 2), cur_time, display.RED, font)
-
-	Image.Image.paste(img, icon, (0, 50))
 
 	# Location Icon
 	icon = Image.open('/home/pi/Documents/projects/python/E-Paper/E-Paper-Notification-Center/E-Paper-Notification-Center/icons/location.png', mode='r').convert('P')
 	icon = icon.resize((20, 20))
 	Image.Image.paste(img, icon, (0, 0))
 
-	draw.text((130, 30), "Temperature:\n    Min:\n    Max:\nPressure:\nHumidity:\nVisibility:\nWind:\n    Speed:\n    Direction:\n    Gust:", display.BLACK, font)
-
 #	img.show()
+
+	pal_img = Image.new('P', (1, 1))
+	pal_img.putpalette((255, 255, 255, 0, 0, 0, 255, 0, 0) + (0, 0, 0)*252)
+
+	img.convert('RGB').quantize(palette=pal_img)
 	display.set_image(img)
 	display.show()
 
 
+'''
